4acdb850dc26447da5110aac599a4012be4dda5c
andrei-koliadenko <iamandriikol@gmail.com>
2021-02-27 09:20:04 +0200

    Add app

---
+++ src/main/java/com/home/twoDatabaseTransaction/DoubleDatabaseTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.home.twoDatabaseTransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleDatabaseTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubleDatabaseTransactionApplication.class, args);
+    }
+
+}
