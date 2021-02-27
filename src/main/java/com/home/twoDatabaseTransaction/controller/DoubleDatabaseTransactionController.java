0821833f1c3f9c70e821d59f62eee91db5f7b2b2
andrei-koliadenko <iamandriikol@gmail.com>
2021-02-27 09:35:45 +0200

    Update controller

--- src/main/java/com/home/twoDatabaseTransaction/controller/DoubleDatabaseTransactionController.java
+++ src/main/java/com/home/twoDatabaseTransaction/controller/DoubleDatabaseTransactionController.java
@@ -1,10 +1,15 @@
 package com.home.twoDatabaseTransaction.controller;
 
+import com.home.twoDatabaseTransaction.dto.CatDto;
+import com.home.twoDatabaseTransaction.dto.CatDtoWithId;
+import com.home.twoDatabaseTransaction.service.DoubleDatabaseTransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
