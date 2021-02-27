5203c9876b0a0aff1270955a15a9c53252a60840
andrei-koliadenko <iamandriikol@gmail.com>
2021-02-27 14:07:40 +0200

    Add mapstruct mapper

---
+++ src/main/java/com/home/twoDatabaseTransaction/mapper/CatMapper.java
@@ -0,0 +1,26 @@
+package com.home.twoDatabaseTransaction.mapper;
+
+import com.home.twoDatabaseTransaction.dto.CatDto;
+import com.home.twoDatabaseTransaction.dto.CatDtoWithId;
+import com.home.twoDatabaseTransaction.entities.CatDoc;
+import com.home.twoDatabaseTransaction.entities.CatTable;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CatMapper {
+    CatMapper INSTANCE = Mappers.getMapper(CatMapper.class);
+
+    @Mapping(source = "id", target = "_id")
+    CatDoc toCatDoc(CatDto cat, String id);
+
+    @Mapping(source = "id", target = "id")
+    CatTable toCatTable(CatDto cat, String id);
+
+    @Mapping(source = "_id", target = "id")
+    CatDtoWithId toCatDto(CatDoc cat);
+
+    CatDtoWithId toCatDto(CatTable cat);
+
+}
